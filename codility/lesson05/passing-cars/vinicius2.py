bc2b243921335ee4fb8ce529fe6a9dcb096b2e76
Vinicius Brenck <viniciusbrenck@hotmail.com>
2022-06-23 23:03:39 -0300

    Vinicius - Passing Cars

---
+++ codility/lesson05/passing-cars/vinicius2.py
@@ -0,0 +1,58 @@
+'''
+A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+
+Array A contains only 0s and/or 1s:
+
+0 represents a car traveling east,
+1 represents a car traveling west.
+The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+
+For example, consider array A such that:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+
+Write a function:
+
+def solution(A)
+
+that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
+
+The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
+
+For example, given:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+the function should return 5, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer that can have one of the following values: 0, 1.
+'''
+
+def solution(A):
+    len_A = len(A)
+    cum_sum = [0] * (len_A+1)
+    for i in range(1, len_A+1):
+        cum_sum[i] = cum_sum[i-1] + A[i-1]
+    num_pairs = 0
+    cum_sum = cum_sum[1:]
+    for index, el in enumerate(A):
+        if el == 0:
+            num_pairs += (cum_sum[-1] - cum_sum[index])
+            if num_pairs > 1_000_000_000:
+                return -1
+    return num_pairs
+
+'''
+RESULTADO 100%
+'''
