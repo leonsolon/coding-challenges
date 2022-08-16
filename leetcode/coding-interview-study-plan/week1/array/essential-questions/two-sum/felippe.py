5e1bc17f7ccd1caecf14fd2455b162de083bb9de
Felippe Aquino de Moura <50746282+faqmoura@users.noreply.github.com>
2022-08-16 10:09:42 -0300

    Update felippe.py

--- leetcode/coding-interview-study-plan/week1/array/essential-questions/two-sum/felippe.py
+++ leetcode/coding-interview-study-plan/week1/array/essential-questions/two-sum/felippe.py
@@ -1,3 +1,8 @@
+# Week 1 - Array
+# Two Sum
+# Runtime: 2395 ms, faster than 39.20% of Python online submissions for Two Sum.
+# Memory Usage: 14.3 MB, less than 45.09% of Python online submissions for Two Sum.
+
 class Solution(object):
     def twoSum(self, nums, target):
     
